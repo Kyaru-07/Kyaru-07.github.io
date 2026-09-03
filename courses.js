e9fe72b249a9f348837040c89ca1c6143619fc01
Kyaru <kyaru_pj@foxmail.com>
2026-09-03 22:00:25 +0800

    Fix Dongguan evening course times

--- courses.js
+++ courses.js
@@ -7,7 +7,7 @@ window.PRESET_COURSES = [
   { weekday: 4, start: '10:25', end: '12:00', name: '软件设计模式', teacher: '欧伟锋讲师', place: '7号楼（7B-401）', weeks: '1-16周' },
   { weekday: 2, start: '10:25', end: '12:00', name: '计算机网络', teacher: '赵慧讲师', place: '7号楼（7B-313）', weeks: '1-16周' },
   { weekday: 2, start: '16:25', end: '18:05', name: '软件测试', teacher: '王洁讲师', place: '6号楼F座（6F-301）', weeks: '1-16周' },
-  { weekday: 2, start: '19:30', end: '21:10', name: 'Java Web应用开发', teacher: '胡传福讲师', place: '6号楼F座（6F-306）', weeks: '1-16周' },
-  { weekday: 5, start: '19:30', end: '21:10', name: 'Qt应用程序开发', teacher: '刘文杰副研究员', place: '7号楼（7B-301）', weeks: '1-16周' },
+  { weekday: 2, start: '19:30', end: '22:00', name: 'Java Web应用开发', teacher: '胡传福讲师', place: '6号楼F座（6F-306）', weeks: '1-16周' },
+  { weekday: 5, start: '19:30', end: '22:00', name: 'Qt应用程序开发', teacher: '刘文杰副研究员', place: '7号楼（7B-301）', weeks: '1-16周' },
   { weekday: 6, start: '14:30', end: '16:10', name: '形势与政策5', teacher: '薛敏霞讲师', place: '待定', weeks: '1-4周' }
 ];
